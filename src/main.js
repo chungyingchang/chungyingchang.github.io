a93f908889d2f5b91aacb16ad9c24707c511a1d4
seed <cychang.dev.tw@gmail.com>
2024-03-30 19:26:36 +0800

    copy previous template

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import { createApp} from 'vue'
-import App from './App.vue'
-
-createApp(App).use(router).mount('#app')
-import 'bootstrap/dist/js/bootstrap.js'
-import router from './router'
-
-import 'mdb-vue-ui-kit/css/mdb.min.css';
